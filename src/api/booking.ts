731d365893d2a69daf1d379cc2dfaa915426b898
tetorgal <hector.galarza.css@gmail.com>
2024-08-14 23:57:08 -0600

    fix: arreglar types para booking.ts

--- src/api/booking.ts
+++ src/api/booking.ts
@@ -1,4 +1,4 @@
-export default async (req, res) => {
+export default async (req: { method: string; body: { service: any; barber: any; date: any; clientName: any; clientEmail: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): void; new(): any; }; }; }) => {
     if (req.method === 'POST') {
       const { service, barber, date, clientName, clientEmail } = req.body;
       // Aquí guardarías la cita en una base de datos, por ejemplo
